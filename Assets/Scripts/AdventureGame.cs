19e6b112136c853307f65b2635fd063b00d6ff32
Hex21189 <hex21189@msn.com>
2022-04-23 21:10:05 +0900

    Initial commit.

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI storyText;
+    [SerializeField] private State startingState;
+
+    private State currentState;
+
+    private void Start()
+    {
+        SetState(startingState);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Application.Quit();
+        }
+
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        State[] nextStates = currentState.GetNextStates();
+
+        for (int i = 0; i < nextStates.Length; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                SetState(nextStates[i]);
+                break;
+            }
+        }
+    }
+
+    private void SetState(State nextState)
+    {
+        currentState = nextState;
+        storyText.text = currentState.GetStateStory();
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [SerializeField] [TextArea(10, 14)] private string storyText;
+    [SerializeField] private State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
